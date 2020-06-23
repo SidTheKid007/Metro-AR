f21e5346bdf5218cea29af629521b0a5b729dd5a
Siddhu K <siddhesvark@gmail.com>
2020-06-23 09:58:34 -0500

    Train asset swap + comments

--- Full Project/scripts/lineSelection.js
+++ Full Project/scripts/lineSelection.js
@@ -1,3 +1,4 @@
+// All module imports
 const Scene = require('Scene');
 const Reactive = require('Reactive');
 const Patches = require('Patches');
@@ -5,16 +6,20 @@ const Diagnostics = require('Diagnostics');
 const Materials = require('Materials');
 const TouchGestures = require('TouchGestures');
 const NativeUI = require('NativeUI');
-//const Networking = require('Networking');
+// const Networking = require('Networking');
 const textures = require("Textures")
 
 const sceneRoot = Scene.root;
+
+// Set up of text used in station confirmation field
 const generalText = 'Are you sure you want \n';
 var pickedStation = 'None'
 
+// Placeholders for the directions to and from data
 var startStation = ''
 var endStation = ''
 
+// PseudoAPI holding the ground truth values for everything
 const stationJSON = {
     'None' : {
         'Food' : 'None',
@@ -27,7 +32,7 @@ const stationJSON = {
     'Arcadia' : {
         'Food' : 'Din Tai Fung',
         'Attraction' : 'RaceTrack',
-        'Time' : 50,
+        'Time' : 110,
         'Directions' : {
         	'Compton' : 'Gold Line:\n Arcadia -> Union Station\nRed Line:\n Union Station -> 7th St\nBlue Line:\n 7th St -> Compton'
         }
@@ -35,20 +40,24 @@ const stationJSON = {
     'Metro Center' : {
         'Food' : 'Hatch Yakitori',
         'Attraction' : 'Fine Arts Building',
-        'Time' : 40,
+        'Time' : 100,
         'Directions' : {
         	'Arcadia' : 'Red Line:\n 7th St -> Union Station\Gold Line:\n Union Station -> Arcadia'
         }
     }
 };
+
+// Storage variables for station information
 var bestFood = ""
 var bestAttraction = ""
 var nextTime = 0
 
+// Flags used to ensure that the UI picker is only visible on the station screen and that the train moves once
 var pickerVisible = false
 var animateOnce = false
 Patches.setBooleanValue('pickerVisible', pickerVisible);
 
+// Reading in time passed and the choice made on pickerUI
 const timePassed = Patches.getScalarValue('timePassed');
 const choicePicked = Patches.getScalarValue('choicePicked');
 
@@ -79,7 +88,6 @@ Promise.all([
     //Other Stations
     sceneRoot.findFirst('select-MetroCenter'),
     sceneRoot.findFirst('otherMapImage')
-    // Add comments later
 ])
 .then(function(objects) {
     const arcadiaBgObj = objects[0];
@@ -92,6 +100,7 @@ Promise.all([
     const stationBox = objects[7];
     const stationText = objects[8];
     const stationTrain = objects[9];
+    // This asset is called 'The Lonely train', and it is made by curupira. View it here: https://sketchfab.com/3d-models/the-lonely-train-6c63871205ed4f5b96ad5c6cde95ea3b
     const trainTransform = stationTrain.transform;
     const fromLocationText = objects[10];
     const toLocationText = objects[11];
@@ -105,23 +114,28 @@ Promise.all([
     const toLocationBox = objects[19];
     const metroCenterBgObj = objects[20];
     const otherMapImage = objects[21];
+    // Connecting and Setting up all the scene assets needed 
 
     TouchGestures.onTap(arcadiaBgObj).subscribe(function (gesture) {
+        // If a user presses on Arcadia station, a popup will ask if you want to go to Arcadia Station
         verifyBox.hidden = false;
         directionsBox.hidden = true;
         verifyText.text = generalText + "Arcadia Station?";
         pickedStation = 'Arcadia';
     });
     TouchGestures.onTap(metroCenterBgObj).subscribe(function (gesture) {
+        // If a user presses on 7th St/Metro Ctr Station, a popup will ask if you want to go to 7th St/Metro Ctr Station
         verifyBox.hidden = false;
         directionsBox.hidden = true;
         verifyText.text = generalText + "Metro Center?";
         pickedStation = 'Metro Center';
     });
     TouchGestures.onTap(noChoice).subscribe(function (gesture) {
+        // If a user presses no on station confirmation popup, the popup will disappear
         verifyBox.hidden = true;
     });
     TouchGestures.onTap(yesChoice).subscribe(function (gesture) {
+        // If a user presses yes on station confirmation popup, the overall map + popup will disappear, and the right station map will show up instead. Also "pseudoAPI" data gets preloaded
         verifyBox.hidden = true;
         overallMap.hidden = true;
         if (pickedStation == 'Arcadia') {
@@ -137,21 +151,24 @@ Promise.all([
         nextTime = stationJSON[pickedStation]['Time'];
     });
     TouchGestures.onTap(fromLocationBox).subscribe(function (gesture) {
+        // If a user presses the "from" location box, the user will be able to edit the text inside
         NativeUI.enterTextEditMode('fromLocationText');
     });
     NativeUI.getText('fromLocationText').monitor().subscribe(function(textUpdate){
+        // This monitors what the user has entered in the "from" location box
     	startStation = textUpdate.newValue;
     });
     TouchGestures.onTap(toLocationBox).subscribe(function (gesture) {
+        // If a user presses the "to" location box, the user will be able to edit the text inside
         NativeUI.enterTextEditMode('toLocationText');
     });
     NativeUI.getText('toLocationText').monitor().subscribe(function(textUpdate){
+        // This monitors what the user has entered in the "to" location box
     	endStation = textUpdate.newValue;
     });
     TouchGestures.onTap(submitDirection).subscribe(function (gesture) {
+        // If a user presses go on the directions box, the directions will be retrieved from the "pseudoAPI" and displayed
         resultingDirectionsBox.hidden = false;
-        // do some json checking
-        //resultingDirectionsText.text = startStation + "\n" + endStation;
         if (stationJSON.hasOwnProperty(startStation)){
         	const fromStationJson = stationJSON[startStation]['Directions'];
         	if (fromStationJson.hasOwnProperty(endStation)){
@@ -166,20 +183,26 @@ Promise.all([
         }
     });
     TouchGestures.onTap(exitDirection).subscribe(function (gesture) {
+        // If a user presses the x on the directions box, the directions box will be hidden
         resultingDirectionsBox.hidden = true;
         directionsBox.hidden = true;
     });
     TouchGestures.onTap(directionSwitch).subscribe(function (gesture) {
+        // If a user presses the directions switch (the turning arrow), the directions box is shown
         directionsBox.hidden = false;
         verifyBox.hidden = true;
     });
     Reactive.monitorMany([timePassed, choicePicked]).subscribe(function(event) {
-    	// clean this by setting the new val things to actual names
-        if (event.newValues["1"] == 0) {
+        // Setting up variables to actively monitor the time passed since the filter was opened and the choice made by the UI picker
+    	var timePassedValue = event.newValues["0"];
+        var choicePickedValue = event.newValues["1"];
+        if (choicePickedValue == 0) {
+            // Picking choice 0 hides the station info box (if it is shown)
         	stationBox.hidden = true;
         }
-        else if (event.newValues["1"] == 1) {
-        	var timeLeft = nextTime - Math.floor(event.newValues["0"]);
+        else if (choicePickedValue == 1) {
+            // Picking choice 1 shows a countdown timer with the minutes and seconds until the next train
+        	var timeLeft = nextTime - Math.floor(timePassedValue);
             var minutes = Math.floor(timeLeft / 60);
             var seconds = (timeLeft - 60*minutes);
             stationBox.hidden = false;
@@ -196,9 +219,10 @@ Promise.all([
             		stationText.text = 'Next Train:\n' + minutes.toString() + ':' + seconds.toString();
             	}
             	else {
+                    // When the countdown timer hits 0, the train moves and docks in the station
             		stationText.text = 'Train has arrived!';
-            		var trainXPos = -.26 + ((1/50) * (event.newValues["0"] - nextTime))
-            		var trainYPos = .26 - ((1/50) * (event.newValues["0"] - nextTime))
+            		var trainXPos = -.26 + ((1/50) * (timePassedValue - nextTime))
+            		var trainYPos = .26 - ((1/50) * (timePassedValue - nextTime))
             		if (trainXPos < .01) {
             			trainTransform.x = trainXPos
             		}
@@ -208,11 +232,13 @@ Promise.all([
             	}
             }
         }
-        else if (event.newValues["1"] == 2) {
+        else if (choicePickedValue == 2) {
+            // Picking choice 2 shows the best attraction in the area by the station
         	stationBox.hidden = false;
             stationText.text = 'Attraction:\n' + bestAttraction;
         }
-        else if (event.newValues["1"] == 3) {
+        else if (choicePickedValue == 3) {
+            // Picking choice 3 shows the best restaraunt in the area by the station
         	stationBox.hidden = false;
             stationText.text = 'Best Food:\n' + bestFood;
         }
